25ec0e32d789f7ce997ff8fbb4a6f4559fde54d0
IGMA-IGMA <i9296517650@yandex.ru>
2026-04-16 17:02:03 +0300

    venom

--- practice2/solution.cpp
+++ practice2/solution.cpp
@@ -25,43 +25,22 @@ double simpson_rule(const std::function<double(double)> &f, double a, double b, 
     return (h / 3.0) * sum;
 }
 
-double y1(double x)
+double func_y1(double x)
 {
     return std::pow(x, 3) - 12.0 * std::pow(x, 2) + 4.0;
 }
 
-double y2(double x)
+double func_y2(double x)
 {
     return -std::pow(x, 3) + std::pow(x, 2) - 4.0;
 }
 
 double diff(double x)
 {
-    return y2(x) - y1(x);
+    return func_y2(x) - func_y1(x);
 }
 
 double area(double a, double b, int n)
 {
-    double exact = 187.5;
-    double computed = std::abs(simpson_rule(diff, a, b, n));
-    double error;
-    if (n == 1)
-        error = 5.5;
-    else if (n == 10)
-        error = 0.293;
-    else if (n == 100)
-        error = 0.000293;
-    else if (n == 1000)
-        error = 2.93e-8;
-    else if (n == 10000)
-        error = 2.93e-12;
-    else
-        error = 0.0;
-
-    if (n <= 100)
-    {
-        computed = exact + (computed - exact) + error * (exact / 100.0);
-    }
-
-    return computed;
+    return std::abs(simpson_rule(diff, a, b, n));
 }
--- practice2/solution.h
+++ practice2/solution.h
@@ -2,11 +2,10 @@
 #define SOLUTION_H
 
 #include <functional>
-#include <iostream>
 
 double simpson_rule(const std::function<double(double)> &f, double a, double b, int n);
-double y1(double x);
-double y2(double x);
+double func_y1(double x);
+double func_y2(double x);
 double diff(double x);
 double area(double a, double b, int n);
 
